d355026e279ee0599917a8d8029cea3d0a4fbf91
Ines Oddo <inesoddo1@gmail.com>
2020-01-07 19:10:46 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get "cocktails", to: "cocktails#index"
-  get "cocktails/:id/cocktails_id", to: "cocktails_id#index"
-  get "cocktails/new", to: "cocktails#new"
-  post "cocktails", to: "cocktails#create"
-  #get "cocktails/:id/edit", to: "cocktails#edit", as: :edit_cocktail
-  #delete "cocktails/:id", to: "cocktails#destroy", as: :delete_cocktail
-  #get "cocktails/:id", to: "cocktails#show", as: :cocktail
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
